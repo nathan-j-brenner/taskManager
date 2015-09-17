f043475665ea6ba28c3172a2fea9dce1cbab130f
nathan-j-brenner <nathan.j.brenner@gmail.com>
2015-09-17 14:11:20 -0700

    delete and edit feature

--- js/controllers/ctrl.js
+++ js/controllers/ctrl.js
@@ -3,5 +3,15 @@ app.controller('ctrl', ['$scope', function($scope){
 	$scope.tasks = [];
 	$scope.addTask = function(){
 		$scope.tasks.push($scope.taskToAdd);
+		$scope.taskToAdd = '';
+	};
+	$scope.deleteTask = function(task){
+		var taskIndex = $scope.tasks.indexOf(task);
+		$scope.tasks.splice(taskIndex, 1);
+	};
+	$scope.editTask = function(task){
+		var taskIndex = $scope.tasks.indexOf(task);
+		$scope.taskToAdd = $scope.tasks[taskIndex];
+		$scope.tasks.splice(taskIndex, 1);
 	};
 }]);
